e8d879207bbe1468318d83c940742fdd4e20a311
Roberto Medina P <robertomedinamv@gmail.com>
2023-06-02 22:44:24 -0400

    Calculadora M4

--- app/src/main/java/com/example/calculadorai3m4/MainActivity.java
+++ app/src/main/java/com/example/calculadorai3m4/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     // se crean las variables para los campos llenados por el usuario a traves de las id
     private EditText et1, et2;
-    // se crea variable para el textview donde irá el resultado pos su id tv1
+    // se crea variable para el textview donde irá el resultado, por su id tv1
     private TextView tv1;
     // se crea variable para el boton
     private Button bt1,bt2,bt3,bt4;
@@ -23,23 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //funcion para findViewById
 // Se crea un metodo que capture el click, se importan las clases button y view
-      /*  bt1 = (Button) findViewById(R.id.bt1);
-        et1=(EditText) findViewById(R.id.et1);
-        et2=(EditText) findViewById(R.id.et2);
-        tv1=(TextView) findViewById(R.id.tv1);*/
+
         vistas();
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // acá meteré el metodo de la suma, se parsean los et
-                //Integer.parseINT pasa a entero
-                //gettext().tostring saca el texto lo pasa a string
-                int suma=Integer.parseInt(et1.getText().toString())+Integer.parseInt(et2.getText().toString());
-                tv1.setText("El resultado es: "+suma);
+        clicks();
 
-
-            }
-    });
 }
 
 //Se hace un metodo que llame textos y boton
@@ -53,7 +40,56 @@ public void vistas (){
     tv1=(TextView) findViewById(R.id.tv1);
 }
 
+// Se hacen los metodos por cada boton
 public void clicks(){
+    bt1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // acá meteré el metodo de la suma, se parsean los et
+            //Integer.parseINT pasa a entero
+            //gettext().tostring saca el texto lo pasa a string
+            int suma=Integer.parseInt(et1.getText().toString())+Integer.parseInt(et2.getText().toString());
+            tv1.setText("El resultado es: "+suma);
+
+
+        }
+    });
+    bt2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // el metodo de la resta
+            int resta=Integer.parseInt(et1.getText().toString())-Integer.parseInt(et2.getText().toString());
+            tv1.setText("El resultado es: "+resta);
+
+
+        }
+    });
+    bt3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //metodo multiplicacion
+            int multiplicacion=Integer.parseInt(et1.getText().toString())*Integer.parseInt(et2.getText().toString());
+            tv1.setText("El resultado es: "+multiplicacion);
+
+
+        }
+    });
+    bt4.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // metodo division con try catch para division por cero
+            try {
+
+
+                int division = Integer.parseInt(et1.getText().toString()) / Integer.parseInt(et2.getText().toString());
+                tv1.setText("El resultado es: " + division);
+            }
+            catch (ArithmeticException e){
+                tv1.setText("No se puede dividir por 0");
+            }
+
+        }
+    });
 
 }
 
